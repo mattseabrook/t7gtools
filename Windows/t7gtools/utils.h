f2fca13f4906ffb3eae1ec55f79490a060129852
Matthew Seabrook <info@mattseabrook.net>
2018-06-24 02:03:53 -0400

    .

--- Windows/t7gtools/utils.h
+++ Windows/t7gtools/utils.h
@@ -5,11 +5,20 @@
 #include <iostream>
 #include <vector>
 
+//-------------------
+
+/*
+=============================================================================
+Utils Class
+=============================================================================
+*/
 class Utils
 {
 public:
 	static std::vector<char> ReadAllBytes(std::string filename);
 	static uint32_t read_u32_le(uint8_t* bytes);
 };
+
+//-------------------
 
 #endif // __UTILS_H
